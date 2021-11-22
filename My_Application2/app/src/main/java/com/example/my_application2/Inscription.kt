392b55485c93cc82416d8eb27b97c1af9b5b524f
KevinB <kevin.boissiere94@gmail.com>
2021-11-22 16:42:50 +0100

    Debug + 404 profil

--- My_Application2/app/src/main/java/com/example/my_application2/Inscription.kt
+++ My_Application2/app/src/main/java/com/example/my_application2/Inscription.kt
@@ -5,21 +5,45 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class Inscription : AppCompatActivity() {
 
+    private lateinit var auth: FirebaseAuth;
+// ...
+// Initialize Firebase Auth
+
+    public override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
+
+
+    }
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_inscription)
 
+        auth = Firebase.auth
+
+
+
         val connected = findViewById<Button>(R.id.Connected);
+
+
+
+
 
         connected.setOnClickListener(){
             Toast.makeText(this, "Inscription vers Connexion", Toast.LENGTH_LONG).show()
             val intent = Intent(this, Connexion::class.java)
             startActivity(intent)
         }
+
+
     }
 }
